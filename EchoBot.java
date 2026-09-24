29b42a5dfc0b88d7d3075e84a54bb421d5c50472
Dmitry152201 <heueuehhshevev@gmail.com>
2026-09-24 15:22:49 +0500

    Update EchoBot.java

--- EchoBot.java
+++ EchoBot.java
@@ -2,17 +2,14 @@ package org.example;
 
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.bots.AbsSender;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 /**
  * Класс Telegram-бота, реализующий логику Long Polling и эхо-ответов.
  */
 public class EchoBot extends TelegramLongPollingBot {
 
-    private final MessageSender messageSender;
+    private final SendTextMessage messageSender;
 
     /**
      * Конструктор бота с настройками подключения.
@@ -21,7 +18,7 @@ public class EchoBot extends TelegramLongPollingBot {
      */
     public EchoBot(DefaultBotOptions options) {
         super(options);
-        this.messageSender = new MessageSender(this);
+        this.messageSender = new SendTextMessage(this);
     }
 
     /**
@@ -61,41 +58,6 @@ public class EchoBot extends TelegramLongPollingBot {
             long chatId = update.getMessage().getChatId();
 
             messageSender.sendTextMessage(chatId, messageText);
-        }
-    }
-
-    /**
-     * Вложенный класс для отправки текстовых сообщений.
-     */
-    public static class MessageSender {
-
-        private final AbsSender bot;
-
-        /**
-         * Конструктор компонента отправки сообщений.
-         *
-         * @param bot экземпляр бота для выполнения вызовов Telegram API
-         */
-        public MessageSender(AbsSender bot) {
-            this.bot = bot;
-        }
-
-        /**
-         * Отправляет текстовое сообщение в указанный чат.
-         *
-         * @param chatId уникальный идентификатор целевого чата
-         * @param text   содержание отправляемого сообщения
-         */
-        public void sendTextMessage(long chatId, String text) {
-            SendMessage message = new SendMessage();
-            message.setChatId(String.valueOf(chatId));
-            message.setText(text);
-
-            try {
-                bot.execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
